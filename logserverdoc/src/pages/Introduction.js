4a6e6cd67364d0e6dd00a8b64d80191ff9cfad26
Mate Racz <raczm0812@gmail.com>
2022-01-08 15:29:19 +0100

    problem content added

--- logserverdoc/src/pages/Introduction.js
+++ logserverdoc/src/pages/Introduction.js
@@ -18,13 +18,13 @@ const Introduction = () => {
                 Jó lenne egy olyan megoldást találni, mellyel ezeket a logokat egy központi rendszerbe lehet gyűjteni, és emberi szem számára kellemes formában lekérdezhetőek, elemzések készíthetőek.
             </p>
             <p className="mt-2">
-                A feladatunk egy központosított naplószerver létrehozása volt a <b> <a href='https://www.linkedin.com/company/cyansecurity/?originalSubdomain=hu'>cyan Security Group</a></b> részére. A cég tesztelésképp 3 szervert és szerverenként 2-2 darab klienst biztosított nekünk Linux (Ubuntu) operációs rendszerrel. A probléma megoldásához kizárólag
-                open source szoftvereket használtunk. A munka három fő fázisból állt melyek a következők:
+                A feladatunk egy központosított naplószerver létrehozása volt a <a className='link-style' target="_blank" rel='noopener noreferrer' href="https://www.linkedin.com/company/cyansecurity/?originalSubdomain=hu">cyan Security Group</a> részére, mely vállalat open-source biztonsági megoldásokat kínál. A cég tesztelésképp 3 szervert és szerverenként 2-2 darab klienst biztosított nekünk <a className='link-style' target="_blank" rel='noopener noreferrer' href='https://ubuntu.com/'>Linux (Ubuntu)</a> operációs rendszerrel. A probléma megoldásához kizárólag
+                nyílt szoftvereket használtunk. A munka három fő fázisból állt melyek a következők:
             </p>
-                <ListGroup className='mt-2'>
-                    <ListGroup.Item>A szoftverek telepítése.</ListGroup.Item>
-                    <ListGroup.Item>A telepített szoftverek konfigurálása, testreszabása.</ListGroup.Item>
-                    <ListGroup.Item>A rendszer tesztelése.</ListGroup.Item>
+                <ListGroup as='ol' numbered className='w-50'>
+                    <ListGroup.Item as='li'>A szoftverek telepítése.</ListGroup.Item>
+                    <ListGroup.Item as='li'>A telepített szoftverek konfigurálása, testreszabása.</ListGroup.Item>
+                    <ListGroup.Item as='li'>A rendszer tesztelése.</ListGroup.Item>
                 </ListGroup>
             <p className="mt-2">
                 A cég rendszerei különböző naplófájlokat generálnak, viszont jelenleg nincs központosított naplószerverük, illetve voltak próbálkozások, de nem volt egyértelmű, hogy melyik technológiai megoldás lenne a legoptimálisabb.
--- logserverdoc/src/pages/Problem.js
+++ logserverdoc/src/pages/Problem.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Badge } from 'react-bootstrap';
+import { Container, Badge, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const Problem = () => {
     return (
@@ -8,8 +8,74 @@ const Problem = () => {
                 <Badge bg="secondary">2.</Badge> Probléma
             </h2>
             <p className='mt-2'>
-                
+                A <b>cyan Security Group</b> azt szeretné, hogy a hálózat sokféle eszközének rendszernaplóit, valamint az egyedi és nem egyedi szoftveres megoldásokat
+                monitorozni (<b>valós időben, indexelve!</b>) tudjuk a magas rendelkezésre állás és gyors hibakezelések érdekében. Ehhez nem elég egy SSH kapcsolattal megkeresni az adott szolgáltatás elérési útját és hosszasan böngészni a naplókat.
+                Erre nincsen idő egy vállalati környezetben.
+                Sokszor a naplósorokat nem is lehet értelmezni (pl. többsoros, bonyolult), ezeket érdemes lenne úgy processzálni, hogy használhatóak legyenek.
+                Egyszerre nézegetni a fájlokat is igen megterhelő lenne, hiszen előfordulhat, hogy több számunkra érdekes forrás is van, mint 4-5 darab, így kényelmesebb lenne egy GUI használata.
             </p>
+            <p className='mt-2'>
+                Ezek alapvetően rendszermérnöki problémák, melyekre léteznek nyílt forráskódú megoldások, így nem kell nekünk megírnunk a sajátunkat, hanem
+                dolgozhatunk a jól bevált költséghatékony technológiákkal. 
+            </p>
+            <p>A munka megkezdése előtt le kellett fektetni a követelményeket, melyek egyeznek a cég elvárásaival,
+                a tapasztalatunkkal és a tárgy teljesítéséhez szükséges feltételekkel. Ezek a következők:
+            </p>
+            <h4>
+                Követelmények
+            </h4>
+                <ListGroup as='ol' numbered className='w-50'>
+                    <ListGroupItem as='li'>
+                        <b>Ismerkedés a Linux alapú operációs rendszerekkel (Ubuntu)</b>
+                        <div>
+                           alap parancsok, mappaszerkezet, szolgáltatások kezelése
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>A naplófájlok, naplózás mechanikájának tanulmányozása</b>
+                        <div>
+                            <code>/var/log/</code> mappa átnézése, syslog sorok
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>Tech stack-ek megismerése</b>
+                        <div>
+                            feladatok elosztása, komponensek dokumentációjának olvasása
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>Komponensek telepítése</b>
+                        <div>
+                            a tech stack-ek telepítése virtuális szerverre, konfigurálás
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>Tesztelés</b>
+                        <div>
+                            loggyűjtés tesztelése localhoston, majd kliensekkel is
+                        </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>Logok lekérdezése</b>
+                       <div>
+                       webes felület használata
+                       </div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                        <b>Vizualizáció</b>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                       <b>Webes felület biztonságossá tétele</b>
+                       <div>reverse proxy megoldása, beépített opciók</div>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                       <b>Logok szűrése</b>
+                    </ListGroupItem>
+                    <ListGroupItem as='li'>
+                       <b>Összehasonlítás</b>
+                    </ListGroupItem>
+                </ListGroup>
+            
         </Container>
     )
 }
