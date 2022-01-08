f6be9f1d0d02ee067beb36c5bae4a769d43ca93d
Mate Racz <raczm0812@gmail.com>
2022-01-08 11:51:28 +0100

    intro content added

--- logserverdoc/src/pages/Introduction.js
+++ logserverdoc/src/pages/Introduction.js
@@ -9,10 +9,12 @@ const Introduction = () => {
             <p className="mt-2">
                 Szinte mindegyik operációs rendszer, alkalmazás generál naplófájlokat. Ezek a fájlok rögzítik az operációs rendszerben bekövetkezett eseményeket,
                 szoftverfuttatásokat, vagy felhasználók közötti üzeneteket. A naplófájlokból rengeteg információt megtudhatunk, ezért is fontosak. Ki / Mi? Mikor? Hol? készítette, mennyire súlyos, vagy esetleg
-                csak informatív jellegű.
+                csak informatív jellegű. Képzeljük, csak el, hogy egy vállalati környezetben működő szerverek, azok szolgáltatásai, webes alkalmazások, asztali alkalmazások mennyi naplófájlt generálnak.
+                Lehetetlenség külön minden forrásra figyelni, reagálni a hibákra.
+                Jó lenne egy olyan megoldást találni, mellyel ezeket a logokat egy központi rendszerbe lehet gyűjteni, és emberi szem számára kellemes formában lekérdezhetőek, elemzések készíthetőek.
             </p>
             <p className="mt-2">
-                A feladatunk egy központosított naplószerver létrehozása volt a <b>cyan Security Group</b> részére. A cég tesztelésképp 3 szervert és szerverenként 2-2db klienst biztosított nekünk Linux operációs rendszerrel. A probléma megoldásához kizárólag
+                A feladatunk egy központosított naplószerver létrehozása volt a <b>cyan Security Group</b> részére. A cég tesztelésképp 3 szervert és szerverenként 2-2db klienst biztosított nekünk Linux (Ubuntu) operációs rendszerrel. A probléma megoldásához kizárólag
                 open source szoftvereket használtunk. A munka három fő fázisból állt melyek a következők:
             </p>
                 <ListGroup className="mt-2">
@@ -21,8 +23,8 @@ const Introduction = () => {
                     <ListGroup.Item>A rendszer tesztelése.</ListGroup.Item>
                 </ListGroup>
             <p className="mt-2">
-                A cég rendszerei különböző naplófájlokat generálnak, viszont jelenleg nincs központosított naplószerverük. A cél egy olyan környezet
-                létrehozása, amely felel a naplófájlok közös gyűjtéséért, hogy azokat a jövőben gördülékenyen lehessen elemezni.
+                A cég rendszerei különböző naplófájlokat generálnak, viszont jelenleg nincs központosított naplószerverük, illetve voltak próbálkozások, de nem volt egyértelmű, hogy melyik technológiai megoldás lenne a legoptimálisabb.
+                A cél egy olyan környezetlétrehozása, amely felel a naplófájlok közös gyűjtéséért, hogy azokat a jövőben gördülékenyen lehessen elemezni.
             </p>
             <p className="mt-2">
                 A feladatot hárman csináltuk (Rácz Máté, Mandl Ábel, Horváth Bálint) és mindhárman különböző modern és ingyenes technológiával dolgoztunk.
