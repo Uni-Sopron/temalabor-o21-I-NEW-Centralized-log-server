3e4c806cee0aee563bb4226d550503f53482c4e5
Ábel Mandl <abelmandl00@gmail.com>
2022-01-15 16:09:56 +0100

    tech-table

--- logserverdoc/src/pages/Tech.js
+++ logserverdoc/src/pages/Tech.js
@@ -1,14 +1,91 @@
 import React from 'react';
-import { Container, Badge } from 'react-bootstrap';
+import { Container, Badge, Table } from 'react-bootstrap';
 
 const Tech = () => {
+    const fluentd_conf = "<match logtype.error>type...</match>"
     return (
         <Container className='mt-5'>
             <h2>
                 <Badge bg="secondary">3.</Badge> Technológia
             </h2>
             <p className='mt-2'>
+                <h3>
+                    EFK Stack
+                </h3>
+                <p>
+                    Az EFK Stack, az ELK-hoz hasonló központosított loggyűjtő és megjelenítő megoldás. A három open-source program (elasticsearch, logstash, kibana) közül ugyanis
+                    itt kimarad a logstash, helyét a <b>fluentd</b> váltja fel.
+                    <Table striped bordered hover style={{ marginTop:20}}>
+                    <thead>
+                        <tr>
+                        <th></th>
+                        <th>Logstash</th>
+                        <th>Fluentd</th>
+                        <th>Megjegyzés</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><b>Fejlesztő</b></td>
+                            <td>A Logstash a híres ELK stack része.</td>
+                            <td>A Fluentd-t a Treasure Data hozta létre.</td>
+                            <td>Ennek ellenére a Fluentd is kiváló támogatást kap az Elastichoz. Támogatása miatt a konténeres technológiákhoz jobb választás, mint a Logstash.</td>
+                        </tr>
 
+                        <tr>
+                            <td><b>Programnyelv</b></td>
+                            <td>JRuby, szükséges Java runtime.</td>
+                            <td>CRuby, nincs szükség a Javara.</td>
+                            <td>A Fluentd előnye, hogy nem használja a Javat.</td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Programnyelv</b></td>
+                            <td>JRuby, szükséges Java runtime.</td>
+                            <td>CRuby, nincs szükség a Javara.</td>
+                            <td>A Fluentd előnye, hogy nem használja a Javat.</td>
+                        </tr>
+                        <tr>
+                            <td><b>Eseménykezelés</b></td>
+                            <td>If, else kondíciók pl. <code>if[loglevel] == 'ERROR'</code></td>
+                            <td>Tageket használ az eseménykezelésre. <code>{fluentd_conf}</code></td>
+                            <td>A Fluentd előnye, hogy tageket használva egyszerűbb a rengeteg típusú log feldolgozására. A Logstashnél kicsit körülményesebb.</td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Plugin támogatás</b></td>
+                            <td>Megközelítőleg 200db.</td>
+                            <td>Megközelítőleg 500db.</td>
+                            <td>A Logstash az összes pluginját központosítva tárolja a github repoban. A Fluentd nem rendelkezik repoval a pluginokhoz.</td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Teljesítmény</b></td>
+                            <td>Több memóriát használ.</td>
+                            <td>Kevesebb memóriát használ.</td>
+                            <td>A Fluentd így jobb teljesítményt biztosít, de igazából teljesítményben mindkét megoldás kézenfekvő.</td>
+                        </tr>
+
+                        <tr>
+                            <td><b>Szállítás</b></td>
+                            <td>A Logstash egy memórián belüli sorra korlátozódik, amely 20 eseményt tartalmaz.</td>
+                            <td>Fluentd egy alaposan konfigurálható Buffer-system-el rendelkezik.</td>
+                            <td>Egy 2015-ös Openstack felmérés szerint mindkét megoldás hasonló és kézenfekvő.</td>
+                        </tr>
+                        
+                        <tr>
+                            <td><b>Támogatás</b></td>
+                            <td>Igen <a href="https://www.elastic.co/guide/en/logstash/current/index.html" target="_blank">Link</a>.</td>
+                            <td>Igen <a href="https://docs.fluentd.org/" target="_blank">Link</a>.</td>
+                            <td>A Logstash az Elastic részeként jobb támogatással rendelkezik.</td>
+                        </tr>
+
+                    </tbody>
+                    </Table>
+                </p>
+                <p>
+                    ...
+                </p>
             </p>
         </Container>
     )
--- logserverdoc/src/pages/Timeline.js
+++ logserverdoc/src/pages/Timeline.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import { Container, Badge } from 'react-bootstrap';
-
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {  FaRecordVinyl } from 'react-icons/fa';
 const Timeline = () => {
     return (
         <Container className='mt-5'>
             <h2>
                 <Badge bg="secondary">6.</Badge> Munkafolyamatok
             </h2>
-            <p className='mt-2'>
-
+            <VerticalTimeline>
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(0, 0, 0)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(108, 117, 125)' }}
+            date="2021. október"
+            iconStyle={{ background: 'rgb(108, 117, 125)', color: '#fff' }}
+            icon={<FaRecordVinyl color="white"/>}
+        >
+            <h3 className="vertical-timeline-element-title">Linux alapismeretek</h3>
+            <h4 className="vertical-timeline-element-subtitle">Parancsok, könyvtárak megismerése</h4>
+            <p>
+            A feladat elvégzéséhez alapvető Linux ismeretekre van szükség. Ilyen volt a legáltalánosabb linux parancsok megismerése is, mint például
+            a <code>cd, ls, cat, cp, rm, mkdir, mv, rmdir</code>.
             </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(0, 0, 0)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(108, 117, 125)' }}
+            date="2021. október"
+            iconStyle={{ background: 'rgb(108, 117, 125)', color: '#fff' }}
+            icon={<FaRecordVinyl color="white"/>}
+        >
+            <h3 className="vertical-timeline-element-title">Linux rendszerlogok</h3>
+            <h4 className="vertical-timeline-element-subtitle">A var/log mappa</h4>
+            <p>
+            A linux általános megismerése után fókuszálhattunk a naplófájlokra, hiszen a feladatban is ezek játszák a főszerepet. A <code>/var/log</code> mappa tartalmát
+            vizsgáltuk meg, hogy a mi esetünkben az <b>Ubuntu</b>, hogyan tárolja el az alap rendszer által generált logokat.
+            Itt megismertük a következő rendszer-log típusokat: <code>syslog, authlog, daemonlog, kernellog, debuglog</code>.
+            </p>
+        </VerticalTimelineElement>
+        </VerticalTimeline>
         </Container>
     )
 }
